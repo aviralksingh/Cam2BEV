5a73290ac2000b9f4246d01e2c38d06a65737399
AviralSingh <aviralksingh@gmail.com>
2020-07-06 16:30:55 -0700

    Added occlusions.sh

---
+++ preprocessing/occlusion/occlusions.sh
@@ -0,0 +1,8 @@
+python3 occlusion.py \
+    --batch ../../data/cam2bev-data/1_FRLR/train/bev \
+    --output ../../data/cam2bev-data/1_FRLR/train/bev+occlusion \
+    ../camera_configs/1_FRLR/drone.yaml \
+    ../camera_configs/1_FRLR/front.yaml \
+    ../camera_configs/1_FRLR/rear.yaml \
+    ../camera_configs/1_FRLR/left.yaml \
+    ../camera_configs/1_FRLR/right.yaml
