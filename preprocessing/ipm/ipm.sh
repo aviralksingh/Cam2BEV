6055c8c430632222bbf829f78f1aebbc8339a2bd
AviralSingh <aviralksingh@gmail.com>
2020-07-06 16:45:24 -0700

    Added ipm.sh

---
+++ preprocessing/ipm/ipm.sh
@@ -0,0 +1,11 @@
+python3 ipm.py --batch --cc \
+    --output ../../data/1_FRLR/train/homography \
+    --drone ../camera_configs/1_FRLR/drone.yaml \
+    ../camera_configs/1_FRLR/front.yaml \
+    ../../data/cam2bev-data/1_FRLR/train/front \
+    ../camera_configs/1_FRLR/rear.yaml \
+    ../../data/cam2bev-data/1_FRLR/train/rear \
+    ../camera_configs/1_FRLR/left.yaml \
+    ../../data/cam2bev-data/1_FRLR/train/left \
+    ../camera_configs/1_FRLR/right.yaml \
+    ../../data/cam2bev-data/1_FRLR/train/right
